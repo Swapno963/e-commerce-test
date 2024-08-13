e7a75f2ba78568a6d1362b213dbe2e793b471a5a
swapno963 <swapno963@gmail.com>
2024-08-13 16:01:20 +0600

    Removed extra / in url

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 export const getUrl = () => {
   // return "http://127.0.0.1:8000";
-  return "https://product-market-backend-test.vercel.app/";
+  return "https://product-market-backend-test.vercel.app";
 };
